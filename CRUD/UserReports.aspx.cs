f22caade967d431357f1e16c6ff4dcf3a96478ae
Robert Steven Tomas <steven.tomas@live.com>
2016-09-27 13:27:58 +0800

    no message

---
+++ CRUD/UserReports.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CrystalDecisions.CrystalReports.Engine;
+
+public partial class UserReports : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        GetUsers();
+    }
+
+    void GetUsers()
+    {
+        ReportDocument report = new ReportDocument();
+        report.Load(Server.MapPath("rptUsers.rpt"));
+        report.DataSourceConnections[0].SetConnection(@"LEMONDEV\LEMONDEV", "CITRINESAIS", true);
+        crvUsers.ReportSource = report;
+        crvUsers.DataBind();
+    }
+}
